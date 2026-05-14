d99362907466a24f80225135e3cf22f7ab533c50
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-05-15 00:28:53 +0500

    first work done

---
+++ apps.js
@@ -0,0 +1,50 @@
+
+// let names = ['noman' , "ali" , 'rizwan' , 45 , true , 200 , 50 , 'yousuf' , 'anas']
+
+
+// // console.log(names)
+
+// names.splice(2 , 3 , 200 , false, 90 )
+
+// console.log(names)
+
+// let copy =names.slice( 1 , 5)
+
+// console.log(copy)
+
+// console.log(names)
+   
+// console.log(names)
+// console.log(names[3])
+// names.push(false)
+
+// names.pop()
+// names.shift()
+// console.log(names)
+
+// names.unshift('pakistan')
+
+
+
+// let username = ["Abdullah" , "Usman" , "Ammar" , "Affan"]; 
+
+// console.log(typeof username);
+// length 1
+// index 0
+// console.log(username[1]);
+
+//  let mixedArr = [1 , 2 ,true , "abdullah" , ['ali' , ['Usman']]]
+
+//  console.log(mixedArr[4][1][0]);
+ 
+
+
+// push
+// pop
+// unshift
+// shift
+// splice
+// slice
+
+// let fruits = ['apple' , 'banana' , 'orange']
+// fruits.push("apple")
